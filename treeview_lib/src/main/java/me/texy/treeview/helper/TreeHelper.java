8dc392cb3fc5b06f73a113b420d08b27140b5c7b
shineM <1092577228@qq.com>
2017-04-30 00:32:48 +0800

    fix selectNode bug

--- treeview_lib/src/main/java/me/texy/treeview/helper/TreeHelper.java
+++ treeview_lib/src/main/java/me/texy/treeview/helper/TreeHelper.java
@@ -190,6 +190,8 @@ public class TreeHelper {
             return expandChildren;
         }
 
+        treeNode.setSelected(select);
+
         if (!treeNode.hasChild()) {
             return expandChildren;
         }
